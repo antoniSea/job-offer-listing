3b9974ddbc8ffe66b8b1c3dc851924dead50e84a
Antoni Seba <antoniwoj@o2.pl>
2022-12-21 16:18:14 +0100

    Add footer to offer show page

---
+++ resources/js/Components/Foter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
+        <span class="text-sm text-gray-500 sm:text-center">© 2022. Wszystkie prawa zastrzezone. Wykonanie <a class="text-blue-500" href="https://github.com/antonisea">Antoni Seba</a>
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Contact</a>
+            </li>
+        </ul>
+    </footer>
+</template>
