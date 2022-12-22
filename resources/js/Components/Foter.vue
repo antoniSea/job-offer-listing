0424ce602ef50f1ceb0ea6380152bdc762b18728
Antoni Seba <antoniwoj@o2.pl>
2022-12-22 18:57:32 +0100

    Update footer info

--- resources/js/Components/Foter.vue
+++ resources/js/Components/Foter.vue
@@ -1,19 +1,14 @@
+<script setup>
+    import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
 <template>
     <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
         <span class="text-sm text-gray-500 sm:text-center">© 2022. Wszystkie prawa zastrzezone. Wykonanie <a class="text-blue-500" href="https://github.com/antonisea">Antoni Seba</a>
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
             <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
+                <Link :href="route('offers.front.index')" class="mr-4 hover:underline md:mr-6 ">Lista ofert</Link>
             </li>
         </ul>
     </footer>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,7 @@
 <script setup>
-    import { Head, Link } from '@inertiajs/inertia-vue3';
-    import Navbar from '../Components/Navbar.vue';
+    import { Head, Link } from '@inertiajs/inertia-vue3'
+    import Navbar from '../Components/Navbar.vue'
+    import Foter from '../Components/Foter.vue'
 
     defineProps({
         canLogin: Boolean,
@@ -55,24 +56,7 @@
         </div>
 
 
-        <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
-            <span class="text-sm text-gray-500 sm:text-center">© 2022. Wszystkie prawa zastrzezone. Wykonanie <a class="text-blue-500" href="https://github.com/antonisea">Antoni Seba</a>
-            </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
-                <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-                </li>
-                <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </footer>
+        <Foter />
 
 
     </div>
