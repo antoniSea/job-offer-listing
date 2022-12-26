f85fba557e1ed7d8d9e2077cede43b69e313570b
Antoni Seba <antoniwoj@o2.pl>
2022-12-26 12:14:11 +0100

    Add show in panel button in offer show view

--- resources/js/Pages/Comments/Index.vue
+++ resources/js/Pages/Comments/Index.vue
@@ -26,7 +26,7 @@
                         <div class="flex justify-between">
                             <Link v-if="!searched_offer_id" method="POST" class="inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 active:bg-blue-600 disabled:opacity-25 transition mb-4" :href="route('comments.change-view-mode')">Zmień na widok ofert</Link>
                             <Link v-else method="POST" class="inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 active:bg-blue-600 disabled:opacity-25 transition mb-4" :href="route('comments.change-view-mode')">Zmień na widok wszystkich komentarzy</Link>
-                        
+
                             <a class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:ring focus:ring-green-200 active:bg-green-600 disabled:opacity-25 transition mb-4" :href="route('comments.pdf')" target="_blank">Generuj plik pdf</a>
                         </div>
                             
