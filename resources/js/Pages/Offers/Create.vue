8d8dc6fb6816ba1858cc5840d30d3c4bff01719e
Antoni Seba <antoniwoj@o2.pl>
2022-12-27 21:06:15 +0100

    Fix image removing in offer creation

--- resources/js/Pages/Offers/Create.vue
+++ resources/js/Pages/Offers/Create.vue
@@ -269,8 +269,8 @@
                         </div>
                         <!-- Add multiple images -->
                         <div ref="parent" class="flex gap-4 overflow-y-hidden overflow-x-scroll">
-                            <div  v-for="url in form.images">
-                                <button @click="removeImage(url)">
+                            <div v-for="url in form.images">
+                                <button @click.prevent="removeImage(url)">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
